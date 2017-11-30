5e799b1a1c275b94adaa110e9f951fffe3f131ae
nicloay <nicloay@gmail.com>
2017-11-30 14:02:53 +0100

    refactoring

--- Assets/Appirater/NativeLocale/Editor/PostProcess.cs
+++ Assets/Appirater/NativeLocale/Editor/PostProcess.cs
@@ -6,6 +6,9 @@ using ChillyRoom.UnityEditor.iOS.Xcode;
 
 public class PostProcess: MonoBehaviour
 {
+    private const string PluginHome = "Appirater/iOS";
+    private const string SoreKitFramework = "StoreKit.framework";   
+    
     [PostProcessBuildAttribute(9999)]
     public static void OnPostProcessBuild(BuildTarget target, string path)
     {
@@ -17,23 +20,16 @@ public class PostProcess: MonoBehaviour
 
     private static void OnIOSBuild(BuildTarget target, string path)
     {
-        NativeLocale.AddLocalizedStringsIOS(path, Path.Combine(Application.dataPath, "Appirater/iOS"));
+        NativeLocale.AddLocalizedStringsIOS(path, Path.Combine(Application.dataPath, PluginHome));
         AddStoreKitFramework(path);
     }
-
-    private const string SoreKitFramework = "StoreKit.framework";    
+     
     private static void AddStoreKitFramework(string projectPath)
     {        
         
         string projPath = projectPath + "/Unity-iPhone.xcodeproj/project.pbxproj";
         PBXProject proj = new PBXProject();
         proj.ReadFromFile(projPath);
-
-//        if (!proj.ContainsFramework(proj.ProjectGuid(), SoreKitFramework))
-//        {
-//            proj.AddFrameworkToProject(proj.ProjectGuid(), SoreKitFramework, false);
-//        }
-       
        
         foreach (string targetName in new string[] {PBXProject.GetUnityTargetName(), PBXProject.GetUnityTestTargetName()})
         {
